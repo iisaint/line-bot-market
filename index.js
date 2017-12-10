f8dc8787fc19669bc1258daae0ca1daab6d738bb
Yao-Hsin Chen <yaohsin@itri.org.tw>
2017-12-10 08:24:32 +0800

    test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const config = {
 };
 
 // create LINE SDK client
-const client = new line.Client(config);
+const client = new line.Client(config); 
 
 // create Express app
 // about Express itself: https://expressjs.com/
